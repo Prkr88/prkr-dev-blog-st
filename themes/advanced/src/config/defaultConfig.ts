a22736e73936a5f87379329d80f61f61a72f07c0
Prkr88 <matanpr@gmail.com>
2022-08-19 14:48:22 +0300

    initial commit

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -3,22 +3,34 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
+    title: "Prkr Dev Blog", // Homepage title
+    titleShort: "Prkr Dev Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "Prkr Dev Blog", // Website name used for homescreen (PWA) and SEO
     description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
     language: "en", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-    url: "https://example.com", // Domain of your website without the pathPrefix
+    url: "https://prkrdevbloh.com", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
-    rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
+    rssTitle: "Prkr Dev Blog RSS Feed", // Title of the RSS feed
+    copyright: "© Copyright 2022", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
   },
 
   // User configuration
-  user: undefined,
+    // User configuration
+  user: {
+  id: "1",
+  firstName: "Matan",
+  lastName:  "Parker",
+  twitterName: "twitt",
+  linkedIn: "linked",
+  github: "git",
+  email: "matanpr@gmail.com",
+  location: "Israel",
+  about: "Software And Information Engineer",
+  avatar: "/logos/matan-liran-cover.jpeg",
+},
 
   // Organization information used for SEO
   organization: undefined,
